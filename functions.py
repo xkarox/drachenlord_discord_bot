1403fe48f5aa0b67df57275d448bed7a85e6f47e
Adrian Grzybek <92431880+xkarox@users.noreply.github.com>
2022-02-20 21:36:06 +0100

    added /stats command

--- functions.py
+++ functions.py
@@ -1,11 +1,36 @@
 # functions.py --
 
 import requests
-import bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import re
 
-def get_revenue():
-    t = requests.get('https://drachenchronik.com/')
-    soup = BeautifulSoup(r.content, 'html-parser')
-    d = {}
+def get_stats():
+    r = requests.get('https://drachenchronik.com/')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    dataThisMonth = {}
+    dataToday = {}
 
-    d['date'] = soup.select_one('.container .box .color .half')
+    month_HTML = soup.select('.box.color.half')[1].text.strip()
+    month_Data = re.findall('(.*)', month_HTML)
+    for i in month_Data:
+        if i == '': month_Data.remove(i)
+    month_Date = month_Data[0]
+    month_Video = month_Data[1]
+    month_Streams = month_Data[2]
+    month_Money = month_Data[3]
+    #print(f'Date: {month_Date}\nVideos: {month_Video}\nStreams: {month_Streams}\nMoney: {month_Money}')
+
+    day_HTML = soup.select('.box.color.half')[0].text.strip()
+    day_Data = re.findall('(.*)', day_HTML)
+    for i in day_Data:
+        if i == '': day_Data.remove(i)
+    day_Date = day_Data[0]
+    day_Video = day_Data[1]
+    day_Streams = day_Data[2]
+    day_Money = day_Data[3]
+
+
+    return month_Date, month_Video, month_Streams, month_Money, day_Date, day_Video, day_Streams, day_Money
+
+if __name__ == '__main__':
+    get_revenue()
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import discord
 import logging
 from dotenv import load_dotenv
+from functions import get_stats
+from discord.ext import commands, tasks
+from datetime import time, timedelta, datetime
 
 
 def main():
     client = discord.Client()
+
 
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,7 +26,8 @@ def main():
     async def on_ready():
         # Setting `Playing ` status
         await client.change_presence(activity=discord.Game(name="am Lümmelmann rum"))
-        print(f'{client.user} has connected to the Discord!')
+        time = datetime.now().strftime('%H:%M:%S')
+        print(f'[{time}]: {client.user} has connected to the Discord!')
 
     @client.event
     async def on_message(message):
@@ -32,9 +37,21 @@ def main():
         if message.content.startswith('/update'):
             await message.channel.send('Etzala, diese Funktion habe ich noch nicht rausgemeddelt!')
 
+        if message.content.startswith('/stats'):
+            m_Date, m_Video, m_Streams, m_Money, d_Date, d_Video, d_Streams, d_Money = get_stats()
+            await message.channel.send(f'{m_Date}\n{m_Video}\n{m_Streams}\n{m_Money}\n---------------------------------------\n{d_Date}\n{d_Video}\n{d_Streams}\n{d_Money}\n')
+
         if message.content.startswith('/hello'):
             await  message.channel.send('Ich bin ned der Drache ferdammde aggsd!!!111!11!')
 
+    @tasks.loop(seconds=31.0)
+    async def test():
+        if datetime.now().strftime('%H:%M') == time(21, 17):
+            print('f')
+
+    y = datetime.now().strftime('%H:%M:%S')
+    print(y)
+    test.start()
     client.run(TOKEN)
 
 if __name__ == '__main__':
